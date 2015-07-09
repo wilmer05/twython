0e32c39e77256f53758713bea35cd9d8aedbde83
Wilmer Rafael Bandres Hernandez <wilmer@simevensimeven.local>
2015-07-09 18:09:24 -0430

    agregando followers y following

--- API_CHACAO/queries.py
+++ API_CHACAO/queries.py
@@ -14,26 +14,33 @@ def get_retweeters(twitter, msj, since=-1):
   usuarios = set()
   x =0
   while(1):
+    if(x>50):
+      return list(usuarios)
+    x+=1
     try:
       results = twitter.search(q=msj,max_id=since,count=100)
     except:
-      print("Mucho queries")
+      print("Muchos queries")
       return list(usuarios)
- 
-      cnt = 0
-      for tweet in results['statuses']:
-        usuarios.add(tweet['user']['screen_name'])
-        cnt+=1
-      if(cnt==0 or len(usuarios)>100000):
-        return list(usuarios)
-      since = results['statuses'][-1]['id']
+    cnt = 0
+    for tweet in results['statuses']:
+      usuarios.add((tweet['user']['screen_name'],\
+                    str(tweet['user']['id'])))
+      cnt+=1
+    if(cnt==0 or len(usuarios)>100000):
+      return list(usuarios)
+    since = results['statuses'][-1]['id']
 
+
+#Funcion que retorna los ultimos tweets de un usuario de
+#nombre name
 def get_tweets_of_user(twitter, name, cant=300):
     ret = list()
     try:
       results = twitter.get_user_timeline(screen_name=name, count=cant)
     except:
       print("Error al buscar tweets de un usuario")
+      return ret
     for tweet in results:
       ret.append(dict(\
                  [('id'       ,tweet['id_str']),\
@@ -41,6 +48,29 @@ def get_tweets_of_user(twitter, name, cant=300):
                   ('contenido', tweet['text']), \
                   ('fecha'    , tweet['created_at'])]))
     return ret
+
+
+#Funcion que obtiene los id's de usuarios de los followers 
+#de un usuario
+def get_followers(twitter, name, cant=300):
+    try:
+      results = twitter.get_followers_ids(screen_name=name,count=cant)
+    except:
+      print("Error al recuperar followers")
+      return ()
+    return results['ids']
+
+#Funcion que obtiene los id's de usuarios de los que sigue 
+#un usuario
+def get_following(twitter, name, cant=300):
+    try:
+      results = twitter.get_friends_ids(screen_name=name,count=cant)
+    except:
+      print("Error al recuperar followings")
+      return ()
+    return results['ids']
+
+
 
 def f(twitter):
  
@@ -71,3 +101,9 @@ OAUTH_TOKEN_SECRET  = authFile[3]
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 twitter.verify_credentials()
+
+print(get_following(twitter,"wilmerBandres",10))
+print(get_followers(twitter,"wilmerBandres",10))
+print(get_tweets_of_user(twitter,"ludafer",10))
+print(get_retweeters(twitter,"wilmerBandres"))
+#f(twitter)
